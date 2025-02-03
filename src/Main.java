056a1521f6da4d36f1e76793c33635795d15f161
looeydooey866 <looeydooey866@gmail.com>
2025-02-03 12:07:37 +0800

    [][][] []    [][][][] []      [][][][][][][] [][]  [][][][][][][][]      TKI FOREVER

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,5 @@
-
+public class Main{
+    public static void main(String[] args){
+        System.out.printf("Welcome to communist mathematica. I don't have much time, please enter your queries now.\n");
+    }                                                                                                                                                                                                                           public static void printf(String s){System.out.printf(s);}
+}
