9a47643bb7ab30aab1cf7edc161810fe789f2bd9
Soyuz-dev <avon2ramesh@gmail.com>
2025-02-07 12:26:51 +0800

    Updated Polynomial
    Added discriminant function for quadratics

--- src/mathematica/Polynomial.java
+++ src/mathematica/Polynomial.java
@@ -7,11 +7,21 @@ public class Polynomial {
     int order = 0;
     public Polynomial(ArrayList<Double> polynoms){
         this.polynoms = polynoms;
-        this.order = polynoms.size();
+        this.order = polynoms.size()-1;
     }
     public Polynomial(double a, double b, double c){
         this.polynoms.add(c);
         this.polynoms.add(b);
         this.polynoms.add(a);
+        this.order = 2;
+    }
+    public double discriminant() throws Exception{
+        if(this.order == 2){
+            double bsquared = Math.pow(polynoms.get(1), 2);
+            double fourac = 4 * polynoms.get(0) * polynoms.get(2);
+            return bsquared - fourac;
+        } else {
+            throw new Exception("Functionality for this has not been made yet.");
+        }
     }
 }
